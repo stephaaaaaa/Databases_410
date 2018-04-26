7e20dd51cc83e277c4c90bdf3542e8be142b89b2
sadieshirts <sadieshirts@u.boisestate.edu>
2018-04-26 09:45:07 -0600

    Added dates in example data. (Intellij throws a fit, but it works).

--- src/example-data.sql
+++ src/example-data.sql
@@ -1,10 +1,10 @@
 USE todo;
---INSERT INTO task (task_label, due_date, tag) VALUES ('walk dog', 2018-04-28, 'home');
---INSERT INTO task (task_label, due_date, tag) VALUES ('get groceries', 2018-04-16, 'home');
---INSERT INTO task (task_label, due_date, tag) VALUES ('clean room', 2018-05-29, 'home');
---INSERT INTO task (task_label, due_date, tag) VALUES ('complete project 3', 2018-04-29, 'project homework school');
---INSERT INTO task (task_label, tag) VALUES ('buy birthday present for Sarah', 'gifts');
---INSERT INTO task (task_label, due_date, tag) VALUES ('math homework', 2018-05-01, 'school');
+INSERT INTO task (task_label, due_date, tag) VALUES ('walk dog', DATE '2018-04-28', 'home');
+INSERT INTO task (task_label, due_date, tag) VALUES ('get groceries', DATE '2018-04-16', 'home');
+INSERT INTO task (task_label, due_date, tag) VALUES ('clean room', DATE '2018-05-29', 'home');
+INSERT INTO task (task_label, due_date, tag) VALUES ('complete project 3', DATE '2018-04-29', 'project homework school');
+INSERT INTO task (task_label, tag) VALUES ('buy birthday present for Sarah', 'gifts');
+INSERT INTO task (task_label, due_date, tag) VALUES ('math homework', DATE '2018-05-01', 'school');
 INSERT INTO task (task_label) VALUES ('graduation invites');
 INSERT INTO task (task_label) VALUES ('get recipe from Jen');
 INSERT INTO task (task_label) VALUES ('renew drivers license');
