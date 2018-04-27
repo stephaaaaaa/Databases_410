6209ac5b82528e72bbc111e4176087a9db0eac60
sadieshirts <sadieshirts@u.boisestate.edu>
2018-04-27 15:26:04 -0600

    SQL Queries in TaskTracker are completely updated with SQL queries.

--- src/TaskTracker.java
+++ src/TaskTracker.java
@@ -26,17 +26,19 @@ public class TaskTracker {
 
     }
 
-    public Task addTask(String taskName){
-        Task newTask = new Task(taskName);
+    public void addTask(String taskName){
+        //Task newTask = new Task(taskName);
         //newTask.setTaskID(currentTaskID);
         String query = "INSERT INTO task (task_label) values (" + taskName + ");";
         runQuery(query);
         //taskList.add(newTask);
-        return newTask;
+        String returnQuery = "SELECT task_id FROM task WHERE task_label = " + taskName + ";";
+        runQuery(returnQuery);
+        //return ;
     }
 
     // Setting return value to String so an error message can be returned
-    public String setDueDate(int taskNum, String dueDate) throws ParseException{
+    public void setDueDate(int taskNum, String dueDate) throws ParseException{
         Date date_due = new SimpleDateFormat("yyyy-MM-dd").parse(dueDate);
         String query = "UPDATE task SET task.due_date = " + date_due + "WHERE task_id = " + taskNum + ";";
         runQuery(query);				
@@ -45,18 +47,20 @@ public class TaskTracker {
 //                task.setDueDate(date_due);
 //            else return "No tasks in the list correspond to ID value" + taskNum;
 //        }
-        return "";
+       // return "";
     }
 
     public void removeTask(String taskName){
-        for(Task task : taskList){
-            if(task.getTaskLabel().equals(taskName)){
-                taskList.remove();
-            }
-        }
+//        for(Task task : taskList){
+//            if(task.getTaskLabel().equals(taskName)){
+//                taskList.remove();
+//            }
+//        }
+    	String query = "DELET FROM task WHERE task_label = " + taskName + ";";
+    	runQuery(query);
     }
 
-    public String showActiveTasks(){
+    public void showActiveTasks(){
        // for(Task task:taskList){
             //if(task.getActiveStatus() == true) {
             	String query = 
@@ -68,116 +72,155 @@ public class TaskTracker {
                 //activeTasks += task.showDetails();
             //}
        // }
-        return activeTasks;
+       // return activeTasks;
     }
 
-    public String addKeywords(int taskNum, String tags){
-        for(Task task:taskList){
-            if(task.getTaskID() == taskNum)
-                    task.addKeyword(tags);
-            else
-                return "No tasks in the list correspond to ID value" + taskNum + ".";
-        }
-        return "";
+    public void addKeywords(int taskNum, String tags){
+//        for(Task task:taskList){
+//            if(task.getTaskID() == taskNum)
+//                    task.addKeyword(tags);
+//            else
+//                return "No tasks in the list correspond to ID value" + taskNum + ".";
+//        }
+    	String query = "UPDATE task SET task.tag = " + tags + " WHERE task_id = " + taskNum + ";";
+        runQuery(query);
+    	//return "";
     }
 
-    public String markTaskComplete(int taskNum) {
-        for (Task task : taskList) {
-            if (task.getTaskID() == taskNum) {
-                if (task.getCompletionStatus() == false)
-                    task.finishTask();
-                else
-                    return "Task " + taskNum + " is already complete";
-            }else
-                return "No tasks in the list correspond to ID value" + taskNum + ".";
-        }
-        return "";
+    public void markTaskComplete(int taskNum) {
+//        for (Task task : taskList) {
+//            if (task.getTaskID() == taskNum) {
+//                if (task.getCompletionStatus() == false)
+//                    task.finishTask();
+//                else
+//                    return "Task " + taskNum + " is already complete";
+//            }else
+//                return "No tasks in the list correspond to ID value" + taskNum + ".";
+//        }
+    	String query = "UPDATE task SET task.is_complete = 1 WHERE task_id = " + taskNum + ";";
+    	runQuery(query);
+       // return "";
     }
 
-    public String cancelTask(int taskNum){
-        for(Task task:taskList){
-            if(task.getTaskID() == taskNum){
-                task.cancelTask();
-            } else return "No tasks in the list correspond to ID value" + taskNum + ".";
-        }
-        return "";
+    public void cancelTask(int taskNum){
+//        for(Task task:taskList){
+//            if(task.getTaskID() == taskNum){
+//                task.cancelTask();
+//            } else return "No tasks in the list correspond to ID value" + taskNum + ".";
+//        }
+    	String query = "UPDATE task SET task.is_cancelled = 1 WHERE task_id = " + taskNum + ";";
+    	runQuery(query);
+        //return "";
     }
 
-    public String showTask(int taskNum){
-        for(Task task:taskList){
-            if(task.getTaskID() == taskNum){
-                task.showDetails();
-            }else return "No tasks in the list correspond to ID value" + taskNum + ".";
-        }
-        return "";
+    public void showTask(int taskNum){
+//        for(Task task:taskList){
+//            if(task.getTaskID() == taskNum){
+//                task.showDetails();
+//            }else return "No tasks in the list correspond to ID value" + taskNum + ".";
+//        }
+    	String query = "SELECT * FROM task WHERE task.task_id = " + taskNum + ";";
+    	runQuery(query);
+       // return "";
     }
 
-    public String showActiveForTag(String tag){
-        LinkedList<Task> activeTasksAssociated_Tag = new LinkedList<>();
-        for(Task task:taskList){
-            if(task.getKeywords().contains(tag))
-                activeTasksAssociated_Tag.add(task);
-            else return "No tasks in the list have '" + tag + "' tag associated with them.";
-        }
-        return activeTasksAssociated_Tag.toString();
+    public void showActiveForTag(String tag){
+//        LinkedList<Task> activeTasksAssociated_Tag = new LinkedList<>();
+//        for(Task task:taskList){
+//            if(task.getKeywords().contains(tag))
+//                activeTasksAssociated_Tag.add(task);
+//            else return "No tasks in the list have '" + tag + "' tag associated with them.";
+//        }
+    	String query = "SELECT * FROM task "
+    			+ "WHERE task.is_complete = 0 "
+    			+ "AND task.is_cancelled = 0 "
+    			+ "AND task.tag LIKE " + tag + ";";
+    	runQuery(query);
+       // return activeTasksAssociated_Tag.toString();
     }
 
-    public String showCompletedTasks(){
-        String completedTasks = "";
-        for(Task task: taskList){
-            if(task.getCompletionStatus() == true){
-                completedTasks.concat(task.getTaskLabel());
-            }
-        }
-        return completedTasks;
+    public void showCompletedTasks(){
+//        String completedTasks = "";
+//        for(Task task: taskList){
+//            if(task.getCompletionStatus() == true){
+//                completedTasks.concat(task.getTaskLabel());
+//            }
+//        }
+    	String query = "SELECT * FROM task "
+    			+ "WHERE task.is_complete = 1;";
+    	runQuery(query);
+        //return completedTasks;
     }
 
-    public String showOverdueTasks(){
-        String overdueTasks = "";
-        for(Task task:taskList){
-            if(task.getDueDate().before(today))
-                overdueTasks += task.showDetails();
-        }
-        if(overdueTasks.equals(""))
-            return "No overdue tasks.";
-        return "";
+    public void showOverdueTasks(){
+//        String overdueTasks = "";
+//        for(Task task:taskList){
+//            if(task.getDueDate().before(today))
+//                overdueTasks += task.showDetails();
+//        }
+//        if(overdueTasks.equals(""))
+//            return "No overdue tasks.";
+    	String query = "SELECT * FROM task " + 
+    			"WHERE task.due_date < TIMESTAMP() " + 
+    			"AND task.is_complete = 0;";
+    	runQuery(query);
+        //return "";
+    }
+    
+    public void showDueToday() {
+    	String query = "SELECT * FROM task " + 
+    			"WHERE task.due_date = EXTRACT DATE FROM TIMESTAMP();";
+    	runQuery(query);
+    	
     }
 
-    public String showDueSoon(){
-        String dueSoon = "";
-        Date tomorrow = new Date(System.currentTimeMillis() + 86400000);
-        Date inTwoDays = new Date(System.currentTimeMillis() + 86400000 * 2);
-        Date inThreeDays = new Date(System.currentTimeMillis() + 86400000 * 3);
-        for(Task task:taskList){
-            Date dueDate = task.getDueDate();
-            if(dueDate.equals(today) || dueDate.equals(tomorrow) ||
-                    dueDate.equals(inTwoDays) || dueDate.equals(inThreeDays)){
-                dueSoon += task.showDetails();
-            }
-        }
-        if(dueSoon.equals(""))
-            return "No tasks are due within the next three days.";
-        return dueSoon;
+    public void showDueSoon(){
+//        String dueSoon = "";
+//        Date tomorrow = new Date(System.currentTimeMillis() + 86400000);
+//        Date inTwoDays = new Date(System.currentTimeMillis() + 86400000 * 2);
+//        Date inThreeDays = new Date(System.currentTimeMillis() + 86400000 * 3);
+//        for(Task task:taskList){
+//            Date dueDate = task.getDueDate();
+//            if(dueDate.equals(today) || dueDate.equals(tomorrow) ||
+//                    dueDate.equals(inTwoDays) || dueDate.equals(inThreeDays)){
+//                dueSoon += task.showDetails();
+//            }
+//        }
+//        if(dueSoon.equals(""))
+//            return "No tasks are due within the next three days.";
+    	String query = "SELECT * FROM task " + 
+    			"WHERE task.due_date <= DATEADD(day, 3, EXTRACT DATE FROM TIMESTAMP()) " + 
+    			"AND task.due_date >= EXTRACT DATE FROM TIMESTAMP();";
+    	runQuery(query);
+       // return dueSoon;
     }
 
     public String renameTask(int taskNum, String newLabel){
-        for(Task task:taskList){
-            if(task.getTaskID() == taskNum){
-                task.setTaskLabel(newLabel);
-            } else return "No tasks in the list correspond to ID value" + taskNum + ".";
-        }
+//        for(Task task:taskList){
+//            if(task.getTaskID() == taskNum){
+//                task.setTaskLabel(newLabel);
+//            } else return "No tasks in the list correspond to ID value" + taskNum + ".";
+//        }
+    	String query = "UPDATE task " + 
+    			"SET task.label = " + newLabel + 
+    			"WHERE task_id = " + taskNum + ";";
+    	runQuery(query);
         return "";
     }
 
-    public String searchByKeyword(String keyword){
-        String tasksWithKeyword = "";
-        for(Task task:taskList){
-            if(task.getKeywords().contains(keyword))
-                tasksWithKeyword += task.showDetails();
-        }
-        if(tasksWithKeyword.equals(""))
-            return "No tasks in the list contain the keyword: " + keyword + ".";
-        return tasksWithKeyword;
+    public void searchByKeyword(String keyword){
+//        String tasksWithKeyword = "";
+//        for(Task task:taskList){
+//            if(task.getKeywords().contains(keyword))
+//                tasksWithKeyword += task.showDetails();
+//        }
+//        if(tasksWithKeyword.equals(""))
+//            return "No tasks in the list contain the keyword: " + keyword + ".";
+    	
+    	String query = "SELECT * FROM task " + 
+    			"WHERE task.task_label LIKE " + keyword + ";";
+    	runQuery(query);
+       // return tasksWithKeyword;
     }
 
     public static void runQuery(String query) {
