a2a6e7cd3037333dbed61b961fdb03949e55450a
Stephanie Labastida <stephanielabastida@u.boisestate.edu>
2018-04-29 19:44:21 -0600

    Uploading jar files
    IntelliJ won't let me commit them, so I am uploading the jar files here

---
+++ out/production/Databases/schema.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE IF NOT EXISTS todo;
+DROP TABLE IF EXISTS task;
+USE todo;
+
+CREATE TABLE task(
+    task_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    task_label VARCHAR(1000) NOT NULL,
+    is_cancelled BIT NOT NULL DEFAULT 0, -- 1 if cancelled, 0 if not cancelled
+    is_complete BIT NOT NULL DEFAULT 0, -- 1 if is complete, 0 if not complete
+    due_date DATE, -- can be null, but can be added at a later time
+    tag VARCHAR(500),
+    time_stamp TIMESTAMP NOT NULL -- the time that a task was created
+);
